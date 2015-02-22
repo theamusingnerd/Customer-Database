3b5ae6e8ea3620c3446ba3d7691b8067445f4017
Alex Sciortino <theamusingnerd@gmail.com>
2015-02-22 10:29:38 -0700

    Update Customer_Database.rb
    Fixed a bug where a non numerical entry will delete or update the last entered customer.

--- Customer_Database.rb
+++ Customer_Database.rb
@@ -33,20 +33,26 @@ when "update"
 	customers_temp = File.readlines("/home/alex/Customer_Database.dat")
 	#Opens the data file and imports it as array
 	puts "What is the number of whom you would like to update?"
-	update = gets.chomp.to_i
-	puts "Name of customer?"
-	name = gets.chomp
-	puts "Phone number?"
-	phone = gets.chomp
-	puts "Enter notes"
-	notes = gets.chomp
-	customers_temp.delete_at(update -= 1)
-	#Deletes old value
-	customers_temp << "#{name}: #{phone}, #{notes}"
-	#Modify line below with your file path
-	open("/home/alex/Customer_Database.dat", "w") do |f|
-		f.puts customers_temp
-		#Writes array to data file.
+	update = gets.chomp
+	if update.to_i.to_s == update
+		#Ensures the input is a number, ensuring that invalid entries do not delete the last entered customer.
+		update_int = update.to_i
+		puts "Name of customer?"
+		name = gets.chomp
+		puts "Phone number?"
+		phone = gets.chomp
+		puts "Enter notes"
+		notes = gets.chomp
+		customers_temp.delete_at(update_int -= 1)
+		#Deletes old value
+		customers_temp << "#{name}: #{phone}, #{notes}"
+		#Modify line below with your file path
+		open("/home/alex/Customer_Database.dat", "w") do |f|
+			f.puts customers_temp
+			#Writes array to data file.
+		end
+	else
+		puts "Error!"
 	end
 when "delete"
 	display(customers)
@@ -55,12 +61,18 @@ when "delete"
 	customers_temp = File.readlines("/home/alex/Customer_Database.dat")
 	#Imports data file as an array
 	puts "What is the number of whom you would like to delete?"
-	delete = gets.chomp.to_i
-	customers_temp.delete_at(delete -= 1)
-	#Modify line below with your file path
-	open("/home/alex/Customer_Database.dat", "w") do |f|
-		f.puts customers_temp
-		#Writes array to data file
+	delete = gets.chomp
+	if delete.to_i.to_s == delete
+		#Ensures the input is a number, ensuring that invalid entries do not delete the last entered customer.
+		delete_int = delete.to_i
+		customers_temp.delete_at(delete_int -= 1)
+		#Modify line below with your file path
+		open("/home/alex/Customer_Database.dat", "w") do |f|
+			f.puts customers_temp
+			#Writes array to data file
+		end
+	else
+		puts "Error!"
 	end
 when "view"
 	display(customers)
